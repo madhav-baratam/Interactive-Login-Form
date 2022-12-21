6f3aff4704f78070d270cac0dd36af929f3869c7
madhav-baratam <baratam.madhav@gmail.com>
2022-12-21 17:13:34 +0530

    first commit

---
+++ login.js
@@ -0,0 +1,15 @@
+function validate()
+{
+  var username=document.getElementById("username").value;
+  var password=document.getElementById("password").value;
+  if(username=="admin" && password=="12345")
+  {
+    alert("Login succesful");
+    return false;
+  }
+  
+  else
+  {
+    alert("Login faild");
+  }
+}
